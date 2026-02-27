08688e67f3e15bb493367baaf75efb7ea2662bca
CreatorMind AI <bannop2007@gmail.com>
2026-02-27 11:01:32 +0400

    Update utils.js

--- utils.js
+++ utils.js
@@ -2,6 +2,8 @@ function randomScare(jumpscare, overlay){
   if(Math.random() < 0.001){
     jumpscare.play();
     overlay.style.background='rgba(255,0,0,0.5)';
-    setTimeout(()=>overlay.style.background='radial-gradient(circle at center, rgba(0,0,0,0) 150px, rgba(0,0,0,0.95) 300px)', 200);
+    setTimeout(()=>{
+      overlay.style.background='radial-gradient(circle at center, rgba(0,0,0,0) 150px, rgba(0,0,0,0.95) 300px)';
+    }, 200);
   }
 }
