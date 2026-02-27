513130f2b42c92733436a6ede0ffc61ef672d78b
CreatorMind AI <bannop2007@gmail.com>
2026-02-27 10:24:08 +0400

    Create player.js

---
+++ player.js
@@ -0,0 +1,16 @@
+class Player {
+  constructor(camera) {
+    this.camera = camera;
+    this.speed = 0.1;
+    this.keys = {};
+    document.addEventListener('keydown', e => this.keys[e.key.toLowerCase()]=true);
+    document.addEventListener('keyup', e => this.keys[e.key.toLowerCase()]=false);
+  }
+
+  update() {
+    if(this.keys['w']) this.camera.position.z -= this.speed;
+    if(this.keys['s']) this.camera.position.z += this.speed;
+    if(this.keys['a']) this.camera.position.x -= this.speed;
+    if(this.keys['d']) this.camera.position.x += this.speed;
+  }
+}
