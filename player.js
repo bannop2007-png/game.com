65947dc2591d23444ba133ff549a701429116d49
CreatorMind AI <bannop2007@gmail.com>
2026-02-27 10:57:09 +0400

    Update player.js

--- player.js
+++ player.js
@@ -1,7 +1,7 @@
 class Player {
   constructor(camera) {
     this.camera = camera;
-    this.speed = 0.1;
+    this.speed = 0.12;
     this.keys = {};
     document.addEventListener('keydown', e => this.keys[e.key.toLowerCase()]=true);
     document.addEventListener('keyup', e => this.keys[e.key.toLowerCase()]=false);
