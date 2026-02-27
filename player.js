2468379d7538a3b0fa46737c3cc1cf833426d80e
CreatorMind AI <bannop2007@gmail.com>
2026-02-27 11:14:00 +0400

    Update player.js

--- player.js
+++ player.js
@@ -1,16 +1,20 @@
+// ------------------ PLAYER ------------------
 class Player {
   constructor(camera) {
-    this.camera = camera;
-    this.speed = 0.12;
-    this.keys = {};
-    document.addEventListener('keydown', e => this.keys[e.key.toLowerCase()]=true);
-    document.addEventListener('keyup', e => this.keys[e.key.toLowerCase()]=false);
+    this.camera = camera;  // камера, за которой движется игрок
+    this.speed = 0.12;     // скорость движения
+    this.keys = {};        // объект для отслеживания нажатых клавиш
+
+    // События клавиатуры
+    document.addEventListener('keydown', e => this.keys[e.key.toLowerCase()] = true);
+    document.addEventListener('keyup', e => this.keys[e.key.toLowerCase()] = false);
   }
 
+  // Метод обновления позиции камеры по нажатым клавишам
   update() {
-    if(this.keys['w']) this.camera.position.z -= this.speed;
-    if(this.keys['s']) this.camera.position.z += this.speed;
-    if(this.keys['a']) this.camera.position.x -= this.speed;
-    if(this.keys['d']) this.camera.position.x += this.speed;
+    if (this.keys['w']) this.camera.position.z -= this.speed;
+    if (this.keys['s']) this.camera.position.z += this.speed;
+    if (this.keys['a']) this.camera.position.x -= this.speed;
+    if (this.keys['d']) this.camera.position.x += this.speed;
   }
 }
